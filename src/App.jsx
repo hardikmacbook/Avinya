0368b12db63d04664fcce261e58e375a360822fd
blaze <hardikkamaliya0000@gmail.com>
2025-07-02 22:07:16 +0530

    creating footer

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
             <Route path="/cart" element={<Cart/>}/>
             <Route path="*" element={<Error/>}/>
           </Routes>
+          
         </BrowserRouter>
       </CartProvider>
     </>

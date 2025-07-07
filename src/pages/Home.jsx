a5b408b936d71fb3ff9ffdc7f29665e246d1f8ac
blaze <hardikkamaliya0000@gmail.com>
2025-07-07 22:53:42 +0530

    removing lazy load

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,20 @@
-import React, { Suspense, lazy } from 'react'
-import Carousel from '../componets/Carousel' // Hero Section – load early
-
-const ProductCard = lazy(() => import('../context/ProductCard'))
-const OurCoreValue = lazy(() => import('../componets/OurCoreValue'))
-const Review = lazy(() => import('../componets/Review'))
-const Footer = lazy(() => import('../componets/Footer'))
-
+import React from 'react'
+import Carousel from '../componets/Carousel'
+import ProductCard from '../context/ProductCard'
+import OurCoreValue from '../componets/OurCoreValue'
+import Review from '../componets/Review'
+import Footer from '../componets/Footer'
 const Home = () => {
   return (
-    <>
-      <div className='w-full h-screen'>
-        <Carousel />
-        <Suspense>
-          <ProductCard />
-        </Suspense>
-        <Suspense>
-          <OurCoreValue />
-        </Suspense>
-        <Suspense>
-          <Review />
-        </Suspense>
-        <Suspense>
-          <Footer />
-        </Suspense>
-      </div>
-    </>
+   <>
+   <div className='w-full h-screen'>
+   <Carousel/>
+   <ProductCard/>
+   <OurCoreValue/>
+   <Review/>
+   <Footer/>
+   </div>
+   </>
   )
 }
 
